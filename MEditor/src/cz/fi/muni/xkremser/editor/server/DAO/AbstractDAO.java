f0380c44d620e3c079d9254691eadb43a76bf4d5
Jiri Kremser <jiri.kremser@gmail.com>
2011-04-20 16:29:35 +0000

    Support for JDBC connection pooling
    git-svn-id: http://meta-editor.googlecode.com/svn/trunk@149 50e9bae0-2ccb-1508-233a-4bfc89a952fd

--- MEditor/src/cz/fi/muni/xkremser/editor/server/DAO/AbstractDAO.java
+++ MEditor/src/cz/fi/muni/xkremser/editor/server/DAO/AbstractDAO.java
@@ -30,12 +30,16 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.annotation.Resource;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 import org.apache.log4j.Logger;
 
 import com.google.inject.Inject;
 
 import cz.fi.muni.xkremser.editor.server.config.EditorConfiguration;
-import cz.fi.muni.xkremser.editor.server.config.EditorConfigurationImpl;
 import cz.fi.muni.xkremser.editor.server.exception.DatabaseException;
 
 // TODO: Auto-generated Javadoc
@@ -56,24 +60,64 @@ public class AbstractDAO {
 
 	private static final String DRIVER = "org.postgresql.Driver";
 
+	/** Must be the same as in the META-INF/context.xml and WEB-INF/web.xml */
+	private static final String JNDI_DB_POOL_ID = "jdbc/editor";
+
+	private static final int POOLABLE_YES = 1;
+	private static final int POOLABLE_NO = 0;
+	private static int poolable = -1;
+
+	@Resource(name = JNDI_DB_POOL_ID)
+	private DataSource pool;
+
 	/**
 	 * Inits the connection.
 	 * 
 	 * @throws DatabaseException
 	 */
 	private void initConnection() throws DatabaseException {
+		if (poolable != POOLABLE_NO && pool == null) {
+			InitialContext cxt = null;
+			try {
+				cxt = new InitialContext();
+			} catch (NamingException e) {
+				poolable = POOLABLE_NO;
+				LOGGER.warn("Unable to get initial context.", e);
+			}
+			if (cxt != null) {
+				try {
+					pool = (DataSource) cxt.lookup("java:/comp/env/" + JNDI_DB_POOL_ID);
+				} catch (NamingException e) {
+					poolable = POOLABLE_NO;
+					LOGGER.warn("Unable to get connection pool.", e);
+				}
+			}
+		}
+		if (pool == null) { // DI is working and servlet container manages the
+												// connections
+			poolable = POOLABLE_NO;
+			initConnectionWithoutPool();
+		} else {
+			poolable = POOLABLE_YES;
+			try {
+				conn = pool.getConnection();
+			} catch (SQLException ex) {
+				LOGGER.error("Unable to get a connection from connection pool " + JNDI_DB_POOL_ID, ex);
+				throw new DatabaseException("Unable to connect to database.", ex);
+			}
+		}
+	}
+
+	private void initConnectionWithoutPool() throws DatabaseException {
 		try {
 			Class.forName(DRIVER);
 		} catch (ClassNotFoundException ex) {
 			LOGGER.error("Could not find the driver " + DRIVER, ex);
 		}
-		if (conf == null) { // called from GWT module without injection support
-			conf = new EditorConfigurationImpl();
-		}
-		String host = conf.getDBHost();
-		String port = conf.getDBPort();
 		String login = conf.getDBLogin();
 		String password = conf.getDBPassword();
+		String host = conf.getDBHost();
+		String port = conf.getDBPort();
 		String name = conf.getDBName();
 		if (password == null || password.length() < 3) {
 			LOGGER.error("Unable to connect to database at 'jdbc:postgresql://" + host + ":" + port + "/" + name + "' reason: no password set.");
