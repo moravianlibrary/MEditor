358ca6d2207bb5b4b64ea2b538bdfbbf113fd7e5
Jiri Kremser <jiri.kremser@gmail.com>
2012-03-06 17:45:59 +0100

    refactoring (cz.fi.muni.xkremser.ediotr package renamed to cz.mzk.editor)

--- resources/dev/LangConstants-i18n.sh
+++ resources/dev/LangConstants-i18n.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+if [ -z "$GWT_HOME" ] ; then
+	echo "Envrironment variable GWT_HOME is not set"
+	exit 1
+fi
+
 APPDIR=`dirname $0`/../..;
-cp $APPDIR/src/cz/fi/muni/xkremser/editor/client/LangConstants_en.properties $APPDIR/src/cz/fi/muni/xkremser/editor/client/LangConstants.properties
-java -cp "$APPDIR/src:$APPDIR/bin:/home/kremser/install/gwt-2.2.0/gwt-user.jar:/home/kremser/install/gwt-2.2.0/gwt-dev.jar" com.google.gwt.i18n.tools.I18NSync -out $APPDIR/src cz.fi.muni.xkremser.editor.client.LangConstants  ;
+cp $APPDIR/src/cz/mzk/editor/client/LangConstants_en.properties $APPDIR/src/cz/mzk/editor/client/LangConstants.properties
+java -cp "$APPDIR/src:$APPDIR/bin:$GWT_HOME/gwt-user.jar:$GWT_HOME/gwt-dev.jar" com.google.gwt.i18n.tools.I18NSync -out $APPDIR/src cz.mzk.editor.client.LangConstants  ;
 echo "i18n has been generated"
