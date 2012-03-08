1ff4a80b98b3176523344c4454bba5a6410d3958
Jiri Kremser <jiri.kremser@gmail.com>
2012-03-08 15:10:58 +0100

    new mave module introduced (editor-dao)

---
+++ editor-dao/src/main/java/cz/mzk/editor/server/DAO/AbstractDAO.java
@@ -0,0 +1,174 @@
+/*
+ * Metadata Editor
+ * @author Jiri Kremser
+ * 
+ * 
+ * 
+ * Metadata Editor - Rich internet application for editing metadata.
+ * Copyright (C) 2011  Jiri Kremser (kremser@mzk.cz)
+ * Moravian Library in Brno
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * 
+ */
+
+package cz.mzk.editor.server.DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import javax.annotation.Resource;
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.apache.log4j.Logger;
+
+import cz.mzk.editor.server.config.EditorConfiguration;
+
+/**
+ * The Class AbstractDAO.
+ * 
+ * @author Jiri Kremser
+ */
+public abstract class AbstractDAO {
+
+    /** The conn. */
+    private Connection conn = null;
+
+    /** The conf. */
+    @Inject
+    private EditorConfiguration conf;
+
+    /** The logger. */
+    private static final Logger LOGGER = Logger.getLogger(AbstractDAO.class);
+
+    private static final String DRIVER = "org.postgresql.Driver";
+
+    /** Must be the same as in the META-INF/context.xml and WEB-INF/web.xml */
+    private static final String JNDI_DB_POOL_ID = "jdbc/editor";
+
+    private static final int POOLABLE_YES = 1;
+    private static final int POOLABLE_NO = 0;
+    private static int poolable = -1;
+
+    @Resource(name = JNDI_DB_POOL_ID)
+    private DataSource pool;
+
+    /**
+     * Inits the connection.
+     * 
+     * @throws DatabaseException
+     */
+    private void initConnection() throws DatabaseException {
+        if (poolable != POOLABLE_NO && pool == null) {
+            if (conf.isLocalhost()) {
+                poolable = POOLABLE_NO;
+                initConnection();
+                return;
+            }
+            InitialContext cxt = null;
+            try {
+                cxt = new InitialContext();
+            } catch (NamingException e) {
+                poolable = POOLABLE_NO;
+                LOGGER.warn("Unable to get initial context.", e);
+            }
+            if (cxt != null) {
+                try {
+                    pool = (DataSource) cxt.lookup("java:/comp/env/" + JNDI_DB_POOL_ID);
+                } catch (NamingException e) {
+                    poolable = POOLABLE_NO;
+                    LOGGER.warn("Unable to get connection pool.", e);
+                }
+            }
+        }
+        if (poolable == POOLABLE_NO || pool == null) { // DI is working and servlet container manages the
+            // connections
+            poolable = POOLABLE_NO;
+            initConnectionWithoutPool();
+        } else {
+            poolable = POOLABLE_YES;
+            try {
+                conn = pool.getConnection();
+            } catch (SQLException ex) {
+                LOGGER.error("Unable to get a connection from connection pool " + JNDI_DB_POOL_ID, ex);
+                poolable = POOLABLE_NO;
+                initConnectionWithoutPool();
+            }
+        }
+    }
+
+    private void initConnectionWithoutPool() throws DatabaseException {
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException ex) {
+            LOGGER.error("Could not find the driver " + DRIVER, ex);
+        }
+        String login = conf.getDBLogin();
+        String password = conf.getDBPassword();
+        String host = conf.getDBHost();
+        String port = conf.getDBPort();
+        String name = conf.getDBName();
+        if (password == null || password.length() < 3) {
+            LOGGER.error("Unable to connect to database at 'jdbc:postgresql://" + host + ":" + port + "/"
+                    + name + "' reason: no password set.");
+            return;
+        }
+        try {
+            conn =
+                    DriverManager.getConnection("jdbc:postgresql://" + host + ":" + port + "/" + name,
+                                                login,
+                                                password);
+        } catch (SQLException ex) {
+            LOGGER.error("Unable to connect to database at 'jdbc:postgresql://" + host + ":" + port + "/"
+                    + name + "'", ex);
+            throw new DatabaseException("Unable to connect to database.");
+        }
+    }
+
+    /**
+     * Gets the connection.
+     * 
+     * @return the connection
+     * @throws DatabaseException
+     */
+    protected Connection getConnection() throws DatabaseException {
+        if (conn == null) {
+            initConnection();
+        }
+        return conn;
+    }
+
+    /**
+     * Close connection.
+     */
+    protected void closeConnection() {
+        try {
+            if (conn != null) {
+                conn.setAutoCommit(true);
+                conn.close();
+            }
+        } catch (SQLException ex) {
+            LOGGER.error("Connection was not closed", ex);
+        }
+        conn = null;
+    }
+
+}
---
+++ editor-dao/src/main/java/cz/mzk/editor/server/DAO/InputQueueItemDAOImpl.java
@@ -0,0 +1,283 @@
+/*
+ * Metadata Editor
+ * @author Jiri Kremser
+ * 
+ * 
+ * 
+ * Metadata Editor - Rich internet application for editing metadata.
+ * Copyright (C) 2011  Jiri Kremser (kremser@mzk.cz)
+ * Moravian Library in Brno
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * 
+ */
+
+package cz.mzk.editor.server.DAO;
+
+import java.io.File;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import cz.mzk.editor.client.util.ClientUtils;
+import cz.mzk.editor.client.util.Constants;
+import cz.mzk.editor.shared.rpc.InputQueueItem;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class InputQueueItemDAOImpl.
+ */
+public class InputQueueItemDAOImpl
+        extends AbstractDAO
+        implements InputQueueItemDAO {
+
+    /** The Constant DELETE_ALL_ITEMS_STATEMENT. */
+    public static final String DELETE_ALL_ITEMS_STATEMENT = "DELETE FROM " + Constants.TABLE_INPUT_QUEUE_ITEM;
+
+    /** The Constant SELECT_NUMBER_ITEMS_STATEMENT. */
+    public static final String SELECT_NUMBER_ITEMS_STATEMENT = "SELECT count(id) FROM "
+            + Constants.TABLE_INPUT_QUEUE_ITEM;
+
+    /** The Constant INSERT_ITEM_STATEMENT. */
+    public static final String INSERT_ITEM_STATEMENT = "INSERT INTO " + Constants.TABLE_INPUT_QUEUE_ITEM
+            + " (path, barcode, ingested) VALUES ((?),(?),(?))";
+
+    /** The Constant FIND_ITEMS_ON_TOP_LVL_STATEMENT. */
+    public static final String FIND_ITEMS_ON_TOP_LVL_STATEMENT =
+            "SELECT p.path, p.barcode, p.ingested, n.name FROM " + Constants.TABLE_INPUT_QUEUE_ITEM
+                    + " p LEFT JOIN " + Constants.TABLE_INPUT_QUEUE_ITEM_NAME + " n ON(p.path=n.path) "
+                    + "WHERE position('" + File.separator + "' IN trim(leading ((?)) FROM p.path)) = 0";
+
+    /** The Constant FIND_ITEMS_ON_TOP_LVL_STATEMENT_ORDERED. */
+    public static final String FIND_ITEMS_ON_TOP_LVL_STATEMENT_ORDERED = FIND_ITEMS_ON_TOP_LVL_STATEMENT
+            + " ORDER BY p.path";
+
+    /** The Constant FIND_ITEMS_BY_PATH_STATEMENT. */
+    public static final String FIND_ITEMS_BY_PATH_STATEMENT = FIND_ITEMS_ON_TOP_LVL_STATEMENT
+            + " AND p.path LIKE ((?)) ORDER BY p.path";
+
+    public static final String UPDATE_INGEST_INFO = "UPDATE " + Constants.TABLE_INPUT_QUEUE_ITEM
+            + " SET ingested = (?) WHERE path = (?)";
+
+    public static final String SELECT_NAME_ITEM_ID = "SELECT id FROM "
+            + Constants.TABLE_INPUT_QUEUE_ITEM_NAME + " WHERE path=(?)";
+
+    public static final String INSERT_NAME = "INSERT INTO " + Constants.TABLE_INPUT_QUEUE_ITEM_NAME
+            + " (name, path) VALUES ((?),(?))";
+
+    public static final String UPDATE_NAME = "UPDATE " + Constants.TABLE_INPUT_QUEUE_ITEM_NAME
+            + " SET name = (?) WHERE path = (?)";
+
+    private static final Logger LOGGER = Logger.getLogger(InputQueueItemDAOImpl.class);
+
+    /*
+     * (non-Javadoc)
+     * @see cz.mzk.editor.server.DAO.InputQueueItemDAO#updateItems(java
+     * .util.List)
+     */
+    @Override
+    public void updateItems(List<InputQueueItem> toUpdate) throws DatabaseException {
+        if (toUpdate == null) throw new NullPointerException("toUpdate");
+
+        try {
+            getConnection().setAutoCommit(false);
+        } catch (SQLException e) {
+            LOGGER.warn("Unable to set autocommit off", e);
+        }
+        try {
+
+            PreparedStatement deleteSt = getConnection().prepareStatement(DELETE_ALL_ITEMS_STATEMENT);
+            PreparedStatement selectCount = getConnection().prepareStatement(SELECT_NUMBER_ITEMS_STATEMENT);
+
+            ResultSet rs = selectCount.executeQuery();
+            rs.next();
+            int totalBefore = rs.getInt(1);
+            // TX start
+            int deleted = deleteSt.executeUpdate();
+            int updated = 0;
+            for (InputQueueItem item : toUpdate) {
+                updated += getItemInsertStatement(item).executeUpdate();
+            }
+            if (totalBefore == deleted && updated == toUpdate.size()) {
+                getConnection().commit();
+                LOGGER.debug("DB has been updated. Queries: \"" + selectCount + "\" and \"" + deleteSt
+                        + "\".");
+            } else {
+                getConnection().rollback();
+                LOGGER.error("DB has not been updated -> rollback! Queries: \"" + selectCount + "\" and \""
+                        + deleteSt + "\".");
+            }
+            // TX end
+        } catch (SQLException e) {
+            LOGGER.error(e);
+        } finally {
+            closeConnection();
+        }
+
+    }
+
+    /**
+     * Gets the item insert statement.
+     * 
+     * @param item
+     *        the item
+     * @return the item insert statement
+     * @throws DatabaseException
+     */
+    private PreparedStatement getItemInsertStatement(InputQueueItem item) throws DatabaseException {
+        PreparedStatement itemStmt = null;
+        try {
+            itemStmt = getConnection().prepareStatement(INSERT_ITEM_STATEMENT);
+            itemStmt.setString(1, item.getPath());
+            itemStmt.setString(2, item.getBarcode());
+            itemStmt.setBoolean(3, item.getIngestInfo());
+        } catch (SQLException ex) {
+            LOGGER.error("Could not get insert item statement " + itemStmt, ex);
+        }
+        return itemStmt;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see cz.mzk.editor.server.DAO.InputQueueItemDAO#getItems(java .lang
+     * .String)
+     */
+    @Override
+    public ArrayList<InputQueueItem> getItems(String prefix) throws DatabaseException {
+        boolean top = (prefix == null || "".equals(prefix));
+        PreparedStatement findSt = null;
+        ArrayList<InputQueueItem> retList = new ArrayList<InputQueueItem>();
+        try {
+            findSt =
+                    getConnection().prepareStatement(top ? FIND_ITEMS_ON_TOP_LVL_STATEMENT_ORDERED
+                            : FIND_ITEMS_BY_PATH_STATEMENT);
+        } catch (SQLException e) {
+            LOGGER.error("Could not get find items statement " + findSt, e);
+        }
+        try {
+            findSt.setString(1, prefix + '/');
+            if (!top) {
+                findSt.setString(2, '%' + prefix + "/%");
+            }
+
+            ResultSet rs = findSt.executeQuery();
+            while (rs.next()) {
+                retList.add(new InputQueueItem(rs.getString("path"), rs.getString("barcode"), rs
+                        .getBoolean("ingested"), rs.getString("name")));
+            }
+        } catch (SQLException e) {
+            LOGGER.error("Query: " + findSt, e);
+        } finally {
+            closeConnection();
+        }
+        return retList;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+
+    @Override
+    public void updateIngestInfo(boolean ingested, String path) throws DatabaseException {
+        try {
+            getConnection().setAutoCommit(false);
+        } catch (SQLException e) {
+            LOGGER.warn("Unable to set autocommit off", e);
+        }
+        try {
+
+            PreparedStatement updateSt = getConnection().prepareStatement(UPDATE_INGEST_INFO);
+            updateSt.setBoolean(1, ingested);
+            updateSt.setString(2, path);
+            int updated = updateSt.executeUpdate();
+            getConnection().commit();
+
+            if (updated == 1) {
+                LOGGER.debug("DB has been updated. Queries: \"" + updateSt + "\".");
+            } else {
+                LOGGER.error("DB has been updated, with unexpected count of updated lines: " + updated
+                        + ". Queries: \"" + updateSt + "\".");
+            }
+            // TX end
+        } catch (SQLException e) {
+            LOGGER.error(e);
+        } finally {
+            closeConnection();
+        }
+    }
+
+    @Override
+    public void updateName(String path, String name) throws DatabaseException {
+        try {
+            getConnection().setAutoCommit(false);
+        } catch (SQLException e) {
+            LOGGER.warn("Unable to set autocommit off", e);
+        }
+        int duplicate = selectName(path);
+        try {
+
+            PreparedStatement updateSt =
+                    getConnection().prepareStatement((duplicate < 0) ? INSERT_NAME : UPDATE_NAME);
+            updateSt.setString(1, ClientUtils.trimLabel(name, Constants.MAX_LABEL_LENGTH));
+            updateSt.setString(2, path);
+            int updated = updateSt.executeUpdate();
+            getConnection().commit();
+
+            if (updated == 1) {
+                LOGGER.debug("DB has been updated. Queries: \"" + updateSt + "\".");
+            } else {
+                LOGGER.error("DB has been updated, with unexpected count of updated lines: " + updated
+                        + ". Queries: \"" + updateSt + "\".");
+            }
+            // TX end
+        } catch (SQLException e) {
+            LOGGER.error(e);
+        } finally {
+            closeConnection();
+        }
+    }
+
+    private int selectName(String path) throws DatabaseException {
+        try {
+            getConnection().setAutoCommit(false);
+        } catch (SQLException e) {
+            LOGGER.warn("Unable to set autocommit off", e);
+        }
+        PreparedStatement selectSt = null;
+        int id = Integer.MIN_VALUE;
+        try {
+
+            selectSt = getConnection().prepareStatement(SELECT_NAME_ITEM_ID);
+            selectSt.setString(1, path);
+            ResultSet rs = selectSt.executeQuery();
+
+            while (rs.next()) {
+                id = rs.getInt("id");
+            }
+        } catch (SQLException e) {
+            LOGGER.error("Query: " + selectSt, e);
+        } finally {
+            closeConnection();
+        }
+        return id;
+    }
+}
