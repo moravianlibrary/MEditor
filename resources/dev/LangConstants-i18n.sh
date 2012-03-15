f3fc453d1302ae2eb30aec8812b312c5e37c5007
Matous Jobanek <matousjobanek@gmail.com>
2012-03-15 09:16:57 +0100

    The i18n skript has been changed due to mavenization

--- resources/dev/LangConstants-i18n.sh
+++ resources/dev/LangConstants-i18n.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 if [ -z "$GWT_HOME" ] ; then
-	echo "Envrironment variable GWT_HOME is not set"
-	exit 1
+        echo "Envrironment variable GWT_HOME is not set"
+        exit 1
 fi
 
-APPDIR=`dirname $0`/../..;
-cp $APPDIR/src/cz/mzk/editor/client/LangConstants_en.properties $APPDIR/src/cz/mzk/editor/client/LangConstants.properties
-java -cp "$APPDIR/src:$APPDIR/bin:$GWT_HOME/gwt-user.jar:$GWT_HOME/gwt-dev.jar" com.google.gwt.i18n.tools.I18NSync -out $APPDIR/src cz.mzk.editor.client.LangConstants  ;
+APPDIR=`dirname $0`/../../editor-common/editor-common-client;
+PATH_TO_CONSTANTS=/src/main/resources/cz/mzk/editor/client
+PATH_TO_JAVA_CLASS=/target/generated-sources/gwt
+
+cp $APPDIR"$PATH_TO_CONSTANTS"/LangConstants_en.properties $APPDIR"$PATH_TO_CONSTANTS"/LangConstants.properties
+mkdir -p $APPDIR/src/cz/mzk/editor/client
+cp $APPDIR"$PATH_TO_CONSTANTS"/*.properties $APPDIR/src/cz/mzk/editor/client/
+
+java -cp "$APPDIR/src:$APPDIR/bin:$GWT_HOME/gwt-user.jar:$GWT_HOME/gwt-dev.jar" com.google.gwt.i18n.tools.I18NSync -out $APPDIR"$PATH_TO_JAVA_CLASS" cz.mzk$
+
+rm -r $APPDIR/src/cz
+
 echo "i18n has been generated"
