32b8b52381376f40a40e9b8ea49c3930b284d733
Matous Jobanek <matousjobanek@gmail.com>
2012-03-29 17:55:44 +0200

    When a monograph unit was created the MODS stream was not generated - it is fixed, but the elements do not have to be correct (TODO: check it and consult it with Pavla)

--- editor-common/editor-common-server/src/main/java/cz/mzk/editor/server/newObject/MonographUnitBuilder.java
+++ editor-common/editor-common-server/src/main/java/cz/mzk/editor/server/newObject/MonographUnitBuilder.java
@@ -24,6 +24,15 @@
 
 package cz.mzk.editor.server.newObject;
 
+import java.util.List;
+
+import org.dom4j.Element;
+import org.dom4j.Namespace;
+import org.dom4j.QName;
+
+import cz.mzk.editor.client.util.Constants.DATASTREAM_CONTROLGROUP;
+import cz.mzk.editor.client.util.Constants.DATASTREAM_ID;
+import cz.mzk.editor.server.fedora.utils.FoxmlUtils;
 import cz.mzk.editor.shared.domain.DigitalObjectModel;
 import cz.mzk.editor.shared.rpc.NewDigitalObject;
 
@@ -47,8 +56,64 @@ public class MonographUnitBuilder
      */
     @Override
     protected void decorateMODSStream() {
-        // TODO Auto-generated method stub
+        String unitLabel = getLabel();
+        Element modsCollection = FoxmlUtils.createModsCollectionEl();
+        Namespace modsNs = Namespaces.mods;
+        Element mods = modsCollection.addElement(new QName("mods", modsNs));
+        mods.addAttribute("version", "3.3");
+        Element idUrn = mods.addElement(new QName("identifier", modsNs));
+        idUrn.addAttribute("type", "urn");
+        idUrn.addText(getUuid());
 
+        Element titleInfo = mods.addElement(new QName("titleInfo", modsNs));
+        Element title = titleInfo.addElement(new QName("title", modsNs));
+        title.addText(unitLabel);
+
+        //        String typeOfResource = getTypeOfResource();
+        //        if (typeOfResource != null) {
+        //            Element typeOfResourceEl = mods.addElement(new QName("typeOfResource", modsNs));
+        //            typeOfResourceEl.addText(getTypeOfResource());
+        //        }
+
+        //        Element genre = mods.addElement(new QName("genre", modsNs));
+        //        genre.addAttribute("type", "volume");
+
+        Element originInfo = mods.addElement(new QName("originInfo", modsNs));
+        Element issuance = originInfo.addElement(new QName("issuance", modsNs));
+        issuance.addText("monographic");
+
+        String language = getLanguage();
+        if (language != null) {
+            Element languageEl = mods.addElement(new QName("language", modsNs));
+            Element languageTerm = languageEl.addElement(new QName("languageTerm", modsNs));
+            languageTerm.addAttribute("type", "code");
+            languageTerm.addAttribute("authority", "iso639-2b");
+            languageTerm.addText(language);
+        }
+        //------  ???????? -----------
+        Element part = mods.addElement(new QName("part", modsNs));
+        part.addAttribute("type", "Chapter");
+
+        Element extent = part.addElement(new QName("extent", modsNs));
+        extent.addAttribute("unit", "pages");
+
+        List<RelsExtRelation> children = getChildren();
+
+        Element start = extent.addElement(new QName("start", modsNs));
+        start.addText(children.get(0).getTargetName());
+        Element end = extent.addElement(new QName("end", modsNs));
+        end.addText(children.get(children.size() - 1).getTargetName());
+        Element total = extent.addElement(new QName("total", modsNs));
+        total.addText(String.valueOf(children.size()));
+        //---------  ???????? --------------
+
+        //        Element part = mods.addElement(new QName("part", modsNs));
+        //        Element detail = part.addElement(new QName("detail", modsNs));
+        //        detail.addAttribute("type", "volume");
+        //        Element number = detail.addElement(new QName("number", modsNs));
+        //        number.addText(unitLabel);
+
+        appendDatastream(DATASTREAM_CONTROLGROUP.X, DATASTREAM_ID.BIBLIO_MODS, modsCollection, null, null);
     }
 
     /**
