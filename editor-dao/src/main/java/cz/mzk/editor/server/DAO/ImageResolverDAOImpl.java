e0cd50133c8db3aa2cd4be2a4d8bd2fee8e29c4a
Matous Jobanek <matousjobanek@gmail.com>
2012-04-24 11:19:14 +0200

    Bug in accessing to the DB has been fixed

--- editor-dao/src/main/java/cz/mzk/editor/server/DAO/ImageResolverDAOImpl.java
+++ editor-dao/src/main/java/cz/mzk/editor/server/DAO/ImageResolverDAOImpl.java
@@ -177,26 +177,35 @@ public class ImageResolverDAOImpl
     @Override
     public String resolveItem(String oldJpgFsPath) throws DatabaseException {
         if (oldJpgFsPath == null || "".equals(oldJpgFsPath)) throw new NullPointerException("oldJpgFsPath");
-        try {
-            getConnection().setAutoCommit(false);
-        } catch (SQLException e) {
-            LOGGER.warn("Unable to set autocommit off", e);
-        }
+
         PreparedStatement statement = null;
         String ret = null;
+        int id = -1;
+
         try {
             // TX start
             statement = getConnection().prepareStatement(SELECT_ITEM_STATEMENT);
             statement.setString(1, oldJpgFsPath);
             ResultSet rs = statement.executeQuery();
             int i = 0;
-            int id = -1;
-            int rowsAffected = 0;
             while (rs.next()) {
                 id = rs.getInt("id");
                 ret = rs.getString("imageFile");
                 i++;
             }
+        } catch (SQLException e) {
+            LOGGER.error(e);
+        } finally {
+            closeConnection();
+        }
+
+        try {
+            getConnection().setAutoCommit(false);
+        } catch (SQLException e) {
+            LOGGER.warn("Unable to set autocommit off", e);
+        }
+        try {
+            int rowsAffected = 0;
             if (id != -1 && new File(ret).exists()) {
                 statement = getConnection().prepareStatement(UPDATE_ITEM_STATEMENT);
                 statement.setInt(1, id);
