868a6fce6509d7e1cdad1d2728bfaa5c3dccc7dc
MartinRumanek <martin@rumanek.cz>
2013-02-20 10:58:58 +0100

    Generated sources from wsdl Abby Recognition Server

---
+++ editor-common/editor-common-server/src/main/java/com/abby/recognitionserver3/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.abbyy.com/RecognitionServer3_xml/RecognitionServer3.xml", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.abby.recognitionserver3;
