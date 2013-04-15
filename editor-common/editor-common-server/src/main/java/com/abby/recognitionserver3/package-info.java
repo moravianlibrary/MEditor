eaf9c8dc4f9ee0f4b9aa90b96bfd6355947b9c05
MartinRumanek <martin@rumanek.cz>
2013-04-15 19:29:11 +0200

    jaxws - generate code from wsdl

--- editor-common/editor-common-server/src/main/java/com/abby/recognitionserver3/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.abbyy.com/RecognitionServer3_xml/RecognitionServer3.xml", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.abby.recognitionserver3;
