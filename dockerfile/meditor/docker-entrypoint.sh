c18897016d23255329bed53e35efc6329fd2ad11
MartinRumanek <martin@rumanek.cz>
2015-05-18 11:16:33 +0200

    Docker - adjustable tomcat password

--- dockerfile/meditor/docker-entrypoint.sh
+++ dockerfile/meditor/docker-entrypoint.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+if [ -n "$TOMCAT_PASSWORD" ] ; then
+  mv /tmp/tomcat-users.xml "$CATALINA_HOME/conf/tomcat-users.xml";
+  sed -i -e "s|\${TOMCAT_PASSWORD}|$TOMCAT_PASSWORD|" \
+     "$CATALINA_HOME/conf/tomcat-users.xml";
+fi
+
+
 exec "$@"
