176366fa8c307f040570ef3127c26c93e5dcfb38
MartinRumanek <martin@rumanek.cz>
2015-05-20 13:09:04 +0200

    Configurable conversion xsl templates Resolve #20

--- dockerfile/meditor/docker-entrypoint.sh
+++ dockerfile/meditor/docker-entrypoint.sh
@@ -5,5 +5,30 @@ if [ -n "$TOMCAT_PASSWORD" ] ; then
      "$CATALINA_HOME/conf/tomcat-users.xml";
 fi
 
+if [ -d /data/meditor/xsl ]; then
+
+  if [ -e /data/meditor/xsl/MARC21slim2MODS3.xsl ]; then
+    rm /root/.meditor/xml/MARC21slim2MODS3.xsl
+  else
+    mv /root/.meditor/xml/MARC21slim2MODS3.xsl /data/meditor/xsl/MARC21slim2MODS3.xsl
+  fi
+  ln -s /data/meditor/xsl/MARC21slim2MODS3.xsl /root/.meditor/xml/MARC21slim2MODS3.xsl
+
+  if [ -e /data/meditor/xsl/MARC21slim2OAIDC.xsl ]; then
+    rm /root/.meditor/xml/MARC21slim2OAIDC.xsl
+  else
+    mv /root/.meditor/xml/MARC21slim2OAIDC.xsl /data/meditor/xsl/MARC21slim2OAIDC.xsl
+  fi
+  ln -s /data/meditor/xsl/MARC21slim2OAIDC.xsl /root/.meditor/xml/MARC21slim2OAIDC.xsl
+
+  if [ -e /data/meditor/xsl/MARC21slimUtils.xsl ]; then
+    rm /root/.meditor/xml/MARC21slimUtils.xsl
+  else
+    mv /root/.meditor/xml/MARC21slimUtils.xsl /data/meditor/xsl/MARC21slimUtils.xsl
+  fi
+  ln -s /data/meditor/xsl/MARC21slimUtils.xsl /root/.meditor/xml/MARC21slimUtils.xsl
+
+fi
+
 
 exec "$@"
