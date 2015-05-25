5fbce2fd543e108a5da17ae32e022285a8deb526
Martin Rumanek <martin@rumanek.cz>
2015-05-25 10:05:00 +0200

    hotfix #27

--- resources/djatoka/bin/compress.sh
+++ resources/djatoka/bin/compress.sh
@@ -69,7 +69,7 @@ NO_PROBLEM=""
 # TIFF_TMP1=`tempfile -s ".tif"`;
   TIFF_TMP2=`tempfile -s ".tif"`;
   echo "converting";
-  convert -colorspace RGB  -compress None $2 $TIFF_TMP2 && {
+  convert  -compress None $2 $TIFF_TMP2 && {
      NO_PROBLEM="ok";
   }
   #&& tiffcp -c none $TIFF_TMP1 $TIFF_TMP2 && rm $TIFF_TMP1 && {
